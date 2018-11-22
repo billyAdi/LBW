a92946551fc1efa3d40f70332a9a052ed24fd62b
billyAdi <billz.wew@gmail.com>
2018-11-22 15:48:48 +0700

    tambahin kode buat get user id

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -45,12 +45,12 @@ public class EchoApplication {
 	boolean modeBoss;
 	HashMap<String, String> hm = new HashMap<String, String>();
 
-    public static void main(String[] args) throws URISyntaxException{
+    public static void main(String[] args){
         SpringApplication.run(EchoApplication.class, args);
     }
 
     @EventMapping
-    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws URISyntaxException{
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event){
         System.out.println("event: " + event);
         String originalMessageText = event.getMessage().getText();
 		String replyText = "";
@@ -85,6 +85,10 @@ public class EchoApplication {
             replyText+=" koneksi gagal";
         }
 		
+		//buat dapetin ID user 
+	    String userId = event.getSource().getUserId();
+		replyText+=" ";
+		replyText+=userId;
         return new TextMessage(replyText);
     }
 
@@ -95,4 +99,27 @@ public class EchoApplication {
 	public String randomText(){
 		return "What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Why do we use it?It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 	}
+	
+	//buat save data
+//	public void saveData(String key,String value, String userID){
+//		Connection conn = null;
+//		try {
+//          conn = DriverManager.getConnection(url, username, password);
+//			String query = "INSERT INTO [NAMA TABEl] VALUES(?,?,?)";
+//			PreparedStatement pst = con.prepareStatement(query)) {
+            
+ //         pst.setString(1, key);
+ //         pst.setString(2, value);
+//			pst.setString(3, userID);
+//          pst.executeUpdate();
+			
+//			conn.close();
+//        } catch (SQLException e) {
+            
+//        }
+	//}
+	
+	
+	
+	
 }
