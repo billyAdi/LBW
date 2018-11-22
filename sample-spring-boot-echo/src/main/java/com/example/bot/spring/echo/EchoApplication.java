e93c88e82ce9c0f3ba7a6be1b5896ec7d7fb87bd
billyAdi <billz.wew@gmail.com>
2018-11-22 22:11:07 +0700

    update

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -34,6 +34,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 @SpringBootApplication
 @LineMessageHandler
